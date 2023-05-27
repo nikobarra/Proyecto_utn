42aa41518f9dcb710e883e49943a0c2beb3ac45c
Nicolas Barra <nicolasbarrapelecano@gmail.com>
2023-05-27 13:08:32 -0300

    genero labels mediante for

--- pro_kiosko.py
+++ pro_kiosko.py
@@ -1,4 +1,4 @@
-from tkinter import Button, Entry, Label, Tk
+from tkinter import Button, Entry, IntVar, Label, Radiobutton, Tk
 
 root = Tk()
 
@@ -8,7 +8,7 @@ def hide_lbl(label):
 
 
 def click_btn_1():
-    txt_lbl = Label(root, text=f'{entry_1.get()} se guardo correctamente')
+    txt_lbl = Label(root, text=f"{entry_1.get()} se guardo correctamente")
     txt_lbl.grid(row=0, column=1)
     root.after(3000, lambda: hide_lbl(txt_lbl))
     entry_1.delete(0, "end")
@@ -19,36 +19,46 @@ def click_btn_1():
     entry_6.delete(0, "end")
 
 
+# Defino los radio button del iva y por default selecciono si
+radio_sel = IntVar()
+radio_sel.set(value=True)
+iva_yes = Radiobutton(root, text="Si", value=True, variable=radio_sel)
+iva_yes.grid(row=2, column=3)
+iva_no = Radiobutton(root, text="No", value=False, variable=radio_sel)
+iva_no.grid(row=2, column=4)
+
 # defino las etiquetas a usar
 
-label_1 = Label(root, text="Producto:")
-label_1.grid(row=0, column=0, sticky="w")
-label_2 = Label(root, text="Descripcion:")
-label_2.grid(row=1, column=0, sticky="w")
-label_3 = Label(root, text="Costo:")
-label_3.grid(row=2, column=0, sticky="w")
-label_4 = Label(root, text="Precio Venta:")
-label_4.grid(row=3, column=0, sticky="w")
-label_5 = Label(root, text="Proveedor:")
-label_5.grid(row=4, column=0, sticky="w")
-label_6 = Label(root, text="Stock:")
-label_6.grid(row=5, column=0, sticky="w")
+labels = ["Producto",
+          "Descripcion",
+          "Costo",
+          "Precio de venta",
+          "Proveedor",
+          "Stock"]
+row_count = 0
+for etiqueta in labels:
+    label_1 = Label(root, text=etiqueta)
+    label_1.grid(row=row_count, column=0, sticky="w")
+    row_count += 1
+label_7 = Label(root, text="incluye iva?:")
+label_7.grid(row=2, column=2, sticky="w")
+
 
 # Defino los campos donde se ingresara el texto
-entry_1 = Entry(root)
+entry_1 = Entry(root, width=100)
 entry_1.grid(row=0, column=1)
-entry_2 = Entry(root)
+entry_2 = Entry(root, width=100)
 entry_2.grid(row=1, column=1)
-entry_3 = Entry(root)
+entry_3 = Entry(root, width=100)
 entry_3.grid(row=2, column=1)
-entry_4 = Entry(root)
+entry_4 = Entry(root, width=100)
 entry_4.grid(row=3, column=1)
-entry_5 = Entry(root)
+entry_5 = Entry(root, width=100)
 entry_5.grid(row=4, column=1)
-entry_6 = Entry(root)
+entry_6 = Entry(root, width=100)
 entry_6.grid(row=5, column=1)
 
 btn_1 = Button(root, text="Agregar", command=click_btn_1)
-btn_1.grid(row=6, column=0)
+btn_1.grid(row=7, column=1)
 
 root.mainloop()
